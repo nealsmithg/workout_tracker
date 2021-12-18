719a111edc35d86a7043388a7c1d7ebee28926a1
Cornelius Smith <nealsmithg@gmail.com>
2021-12-18 16:52:39 -0500

    deploying

--- server.js
+++ server.js
@@ -14,7 +14,15 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout");
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 app.use(routes);
 
